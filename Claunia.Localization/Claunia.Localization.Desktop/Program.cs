abf6e04a3f37b278cdd311b6805a954161e29256
Natalia Portillo <claunia@claunia.com>
2019-05-20 23:51:48 +0100

    Initial commit.

---
+++ Claunia.Localization/Claunia.Localization.Desktop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eto;
+using Eto.Forms;
+
+namespace Claunia.Localization.Desktop
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new Application(Platform.Detect).Run(new MainForm());
+        }
+    }
+}
